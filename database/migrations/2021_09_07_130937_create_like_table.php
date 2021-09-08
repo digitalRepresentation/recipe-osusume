ceacb745a5d3ec6d1f48ffb4a8184e16136f7a2b
digitalRepresentation <kinemura@naver.com>
2021-09-08 19:28:43 +0900

    wip:recipe, picture, fav, like

---
+++ database/migrations/2021_09_07_130937_create_like_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('like', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('recipe_id');
+            $table->boolean('type');
+            $table->timestamps();
+            $table->dateTime('deleted_at', 0)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('like');
+    }
+}
